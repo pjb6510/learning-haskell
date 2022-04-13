77b8805e378252dc330638f0f8718d018dfce97f
ParkJeongbeen <pjb6510@gmail.com>
2022-04-13 22:45:07 +0900

    add: 1.DataTypes.hs

---
+++ 1.DataTypes.hs
@@ -0,0 +1,14 @@
+import Data.List
+import System.IO
+
+-- Int -2^63 ~ 2^63
+maxInt = maxBound :: Int
+
+minInt = minBound :: Int
+
+-- Integer: Unbounded whole number : this value can be as big as memory can hold
+-- Float
+-- Double
+-- Bool: True False
+-- Char '
+-- Tuple
