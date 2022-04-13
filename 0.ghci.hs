2382e4da11d7ef23aedd154511d47a1fe12c80f3
ParkJeongbeen <pjb6510@gmail.com>
2022-04-13 22:44:59 +0900

    add: 0.ghci.hs

---
+++ 0.ghci.hs
@@ -0,0 +1,26 @@
+{-
+
+multi line comment
+
+-}
+
+-- single line comment
+
+{-
+  in terminal,
+
+  ghci
+    execute ghci
+
+  :l [.hs file]
+    load hs file (module)
+
+  :r
+    reload hs file
+
+  :t
+    show type
+
+  :q
+    quit
+-}
