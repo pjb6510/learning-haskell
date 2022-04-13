347b429c3895dded9b9360e797924daf82ae58ba
ParkJeongbeen <pjb6510@gmail.com>
2022-04-13 22:45:17 +0900

    add: 2.MathFunctions.hs

---
+++ 2.MathFunctions.hs
@@ -0,0 +1,78 @@
+import Data.List
+import System.IO
+
+-- sumOfNums = 1+2+3+...1000
+sumOfNums = sum [1 .. 1000]
+
+addEx = 5 + 4
+
+subEx = 5 - 4
+
+multEx = 5 * 4
+
+divEx = 5 / 4
+
+modEx = mod 5 4
+
+-- infix operator
+modEx2 = 5 `mod` 4
+
+negNumEx = 5 + (-4)
+
+-- sqrt:: Floating a => a -> a
+-- have to convert Int to Float
+num9 = 9 :: Int
+
+sqrtOf9 = sqrt (fromIntegral num9)
+
+-- Built in math functions
+piVal = pi
+
+ePow9 = exp 9
+
+logOf9 = log 9
+
+squared9 = 9 ** 2
+
+{-
+  Math.Floor, which rounds down towards negative infinity.
+  Math.Ceiling, which rounds up towards positive infinity.
+  Math.Truncate, which rounds up or down towards zero.
+-}
+truncateVal = truncate 9.999
+
+roundVal = round 9.999
+
+ceilingVal = ceiling 9.999
+
+floorVal = floor 9.999
+
+-- Also sin, cos, tan, asin, atan, acos, sinh,
+-- tanh, cosh, asinh, atanh, acosh
+
+trueAndFalse = True && False
+
+trueOrFalse = True || False
+
+notTrue = not True
+
+{-
+
+Check function type
+Don't be confuse
+
+:t (+)
+  (+) :: Num a => a -> a -> a
+
+Num a is type.
+in typescript,
+  +(operator) = (a:number) => (b:number) => number
+
+:t truncate
+  truncate :: (RealFrac a, Integral b) => a -> b
+
+RealFrac and Integral are type
+in typescript,
+  truncate = (a:RealFrac) => (b:Integral)
+
+-}
