ac2c813ee3b199f6397b7ef7e28402fd9f11d360
ParkJeongbeen <pjb6510@gmail.com>
2022-04-16 16:38:01 +0900

    update: 3.Lists

--- 3.Lists.hs
+++ 3.Lists.hs
@@ -1,4 +1,5 @@
--- List: very very important in haskell
+import Data.List
+import System.IO
 
 primeNumbers = [3, 5, 7, 11]
 
@@ -68,3 +69,44 @@ cycleList = take 13 (cycle [1, 2, 3, 4, 5])
 listTimes2 = [x * 2 | x <- [1 .. 10]]
 
 listChars = [char : "Type" | char <- ['a' .. 'z']]
+
+{-
+  const divsBy9N13 = pipe(
+    range(1, 501),
+    filter((x) => x % 13 === 0),
+    filter((x) => x % 9 === 0),
+  )
+-}
+divisBy9N13 =
+  [ x | x <- [1 .. 500], x `mod` 13 == 0, x `mod` 9 == 0
+  ]
+
+listBiggerThan5 = filter (> 5) morePrimes
+
+sortedList = sort [6, 4, 6, 8, 8, 2, 1, 3, 341, 42, 4, 5, 6]
+
+sumOfLists = zipWith (+) [1, 2, 3, 4, 5] [6, 7, 8, 9, 10]
+
+evensUpTo20 = takeWhile (<= 20) [2, 4 ..]
+
+-- fold
+{-
+  foldr (+) 0 [1,2,3]
+  start to calculate from right value
+
+  1 + (foldr (+) 0 [2,3])
+  1 + (2 + (foldr (+) 0 [3]))
+  1 + (2 + (3 + (foldr (+) 0 [])))
+  1 + (2 + (3 + (0)))
+
+  foldl (+) 0 [1,2,3]
+  foldl (+) (0 + 1) [2,3]
+  foldl (+) (1 + 2) [3]
+  foldl (+) (3 + 3) []
+-}
+
+multiOfList = foldl (*) 1 [2, 3, 4, 5]
+
+sumOfList = foldl (+) 1 [2, 3, 4, 5]
+
+multiTable = [[x * y | x <- [2 .. 9]] | y <- [1 .. 9]]
